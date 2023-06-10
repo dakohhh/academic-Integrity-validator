7a92b9621b15b3aaf340bba8e5b271c32a02a026
Dakoh Wisdom <wiizzydreadmill@gmail.com>
2023-06-10 11:51:36 -1200

    added stopwords

--- utils/similatity_model.py
+++ utils/similatity_model.py
@@ -1,15 +1,22 @@
-import os
-import nltk
 import numpy as np
 from typing import List
 from gensim.models import Word2Vec
 from sklearn.metrics.pairwise import cosine_similarity
-stopwords_filepath = str(nltk.data.find('corpora/stopwords.zip'))
 
-if not os.path.exists(stopwords_filepath):
-    nltk.download('stopwords')
 
-from nltk.corpus import stopwords
+
+stop_words = [
+    'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves',
+    'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their',
+    'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was',
+    'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the',
+    'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against',
+    'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out',
+    'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how',
+    'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own',
+    'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now'
+]
+
 
 
 
@@ -28,7 +35,7 @@ class AssigmeAnalyZer():
         self.model = Word2Vec(self.approved_assignments, min_count=1)
 
 
-        self.stop_words = set(stopwords.words('english'))
+        self.stop_words = set(stop_words)
 
     def preprocess(self, assignment:List[str]) -> List[str]:
 
